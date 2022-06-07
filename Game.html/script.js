cd5f7b073b0a5a7a62c03484ba0820ba0ca7e208
ABHIMCSMPproisbest <107055082+ABHIMCSMPproisbest@users.noreply.github.com>
2022-06-07 23:45:56 +0700

    Add files via upload

---
+++ Game.html/script.js
@@ -0,0 +1,7 @@
+document.onkeydown = function (e) {
+    console.log("e.keyCode")
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino')
+        dino.classList.add('')
+    }
+}
